8581cd1659dbc3b8c61f27b1df86050c6a57ad97
FAL <falworks.contact@gmail.com>
2021-01-14 17:42:08 +0900

    move from assets to src-data

--- src/globals/p5-instance.ts
+++ src/globals/p5-instance.ts
@@ -1,5 +1,6 @@
 import jsdom = require("jsdom");
 import fs = require("fs");
+import dirs = require("../data-directories");
 
 import type * as p5 from "p5";
 
@@ -7,7 +8,7 @@ import type * as p5 from "p5";
  * Asynchronously loads `p5` via CDN and returns its instance.
  */
 export const create = async (): Promise<p5> => {
-  const html = await fs.promises.readFile("assets/load-p5.html");
+  const html = await fs.promises.readFile(`${dirs.srcData}/load-p5.html`);
   const { window } = new jsdom.JSDOM(html, {
     runScripts: "dangerously",
     resources: "usable",
