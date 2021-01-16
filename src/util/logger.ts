99918e9d5a24bb670e2c308569652034211145ea
FAL <falworks.contact@gmail.com>
2021-01-16 14:30:21 +0900

    fix: proper logger functions

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -6,7 +6,15 @@ const prefix = "[eslint-config-p5js-typescript] ";
 export const log = logger.createSimple(prefix, process.stdout);
 
 /** Prints warn message */
-export const warn = logger.createSimple(prefix + "warn:  ", process.stderr);
+export const warn = logger.create(
+  prefix + "warn:  ",
+  process.stderr,
+  console.warn
+);
 
 /** Prints error message. */
-export const error = logger.createSimple(prefix + "ERROR: ", process.stderr);
+export const error = logger.create(
+  prefix + "ERROR: ",
+  process.stderr,
+  console.error
+);
