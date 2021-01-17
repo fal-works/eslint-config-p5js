e7f6f792a9b675a0320c435798fdf1dfecf66977
FAL <falworks.contact@gmail.com>
2021-01-18 01:02:12 +0900

    refactor: minor

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const noUnusedVarsIgnore = require("./data/rules/no-unused-vars/ignore-pattern");
+const noUnusedVarsIgnorePattern = require("./data/rules/no-unused-vars/ignore-pattern");
 const globals = require("./data/globals");
 
 module.exports = {
   rules: {
     "no-unused-vars": [
       "warn",
-      {
-        varsIgnorePattern: noUnusedVarsIgnore,
-      },
+      { varsIgnorePattern: noUnusedVarsIgnorePattern },
     ],
   },
   globals,
